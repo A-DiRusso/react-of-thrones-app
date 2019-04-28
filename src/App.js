560ba37d8c48bfd9199f1816ab7e7132c3f86b7d
Anthony DiRusso <ossurid@gmail.com>
2019-04-28 09:20:18 -0400

    prints object keys to screen, can't currently access the values of those keys :(

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import character from './characters';
+import characters from './characters';
 import CharacterList from './CharacterList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      character: []
+      
     }
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            <CharacterList />
+            <CharacterList characters={Object.keys(characters[0])}/>
+            
         </header>
       </div>
     );
--- src/CharacterList.js
+++ src/CharacterList.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function CharacterList() {
+function CharacterList({characters}) {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ul>
+        {
+            characters.map((name, i) => <li key={i} >{name}{i}</li>)
+        }
+    </ul>
+  
   )
+}
+
+CharacterList.propTypes = {
+    characters: PropTypes.array.isRequired
 }
 
 export default CharacterList;
